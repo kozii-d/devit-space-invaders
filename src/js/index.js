fc2d8a1015e72f4c825cdfec48a52a48eadabef0
Dmitrii Kozii <dkozii1807@gmail.com>
2022-04-17 17:18:20 +0300

    add start modal

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,20 @@ import {Game} from "./game";
 import ('../styles/style.scss');
 
 // todo: поправить костыль с лишним пришельцем
-// todo: поправить регистрацю выстрелов пришельцев (сейчас регистрация происходит по кончику выстрела.
-//  Добавить, чтобы весь выстрел считывался
 // todo: попробовать заменить интервалы на таймауты у пришельцев
 
-let game = new Game();
+let game = null;
 const modal = document.querySelector('.modal');
+const startModal = document.querySelector('.modal_start');
 let modalBnt = document.querySelector('.modal__btn');
+let startModalBtn = document.querySelector('.modal_start__btn');
+
+startModalBtn.addEventListener('click', () => {
+    startModal.style.display = 'none';
+    game = new Game();
+});
+
+
 modalBnt.addEventListener('click', () => {
     modal.style.display = 'none';
     game = game.restartGame();
